5a0ef0643de446341e22dfde192b7b090a23cc35
Rico Eka Rizkya <ricorizkya26@gmail.com>
2022-11-11 21:36:00 +0700

    first commit

--- addons/latihan_purchase_dp/models/purchase.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from odoo import models, fields, api
-from dateutil.relativedelta import relativedelta
-from datetime import datetime,timedelta
-
-class SaleOrder(models.Model):
-    _inherit = "purchase.order"
-
-    down_payment = fields.Float(string="Down Payment")
